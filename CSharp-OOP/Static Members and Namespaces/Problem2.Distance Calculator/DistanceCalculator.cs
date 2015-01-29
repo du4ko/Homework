374eb62f25e045bd0e333cead7ac3d6b6c5bcce0
Viktor <Viktorstanchev7@gmail.com>
2015-01-29 17:01:09 +0200

    Second problem from the homework

---
+++ CSharp-OOP/Static Members and Namespaces/Problem2.Distance Calculator/DistanceCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2.Distance_Calculator
+{
+    static class DistanceCalculator
+    {
+        public static void CalculateDistance(Point3D firstPoint , Point3D secondPoint)
+        {
+            double deltaX = firstPoint.X - secondPoint.X;
+            double deltaY = firstPoint.Y - secondPoint.Y;
+            double deltaZ = firstPoint.Z - firstPoint.Z;
+
+            double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+            Console.WriteLine("The distance between the two points is: {0}" , distance);
+        }
+    }
+}
---
+++ CSharp-OOP/Static Members and Namespaces/Problem2.Distance Calculator/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2.Distance_Calculator
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Point3D firstPoint = new Point3D(1.3, 2.5, 3.6);
+            Point3D secondPoint = new Point3D(3.7, 8.9, 4.6);
+
+            Distance_Calculator.DistanceCalculator.CalculateDistance(firstPoint, secondPoint);
+        }
+    }
+}
