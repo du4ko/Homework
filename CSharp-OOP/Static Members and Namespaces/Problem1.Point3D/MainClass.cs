88d82b9786514c643f1a053645f76d1b2192b2b6
Viktor <Viktorstanchev7@gmail.com>
2015-01-29 16:15:14 +0200

    Second homework plus the first problem of it

---
+++ CSharp-OOP/Static Members and Namespaces/Problem1.Point3D/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Members_and_Namespaces
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Point3D testPoint = new Point3D(1.5, 2.6, 9.7);
+            Console.WriteLine(testPoint.ToString());
+            testPoint = Point3D.StartingPoint;
+            Console.WriteLine(testPoint.ToString());
+        }
+    }
+}
