0b3072092c63552415e9be38c12aa6aceded1280
Viktor <Viktorstanchev7@gmail.com>
2015-02-01 22:19:29 +0200

    Setting up directory for projects outside of Uni

---
+++ OutsideProjects/WriteAndReadFiles/WriteFiles/WriteFiles/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter write = new StreamWriter("testWrite.txt"))
+            {
+                write.Write("Kude e faila");
+            }
+        }
+    }
+}
