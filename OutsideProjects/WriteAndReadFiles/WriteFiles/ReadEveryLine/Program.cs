ecd37301c2492dcd4e633364e6e2312004730319
Viktor <Viktorstanchev7@gmail.com>
2015-02-01 22:37:05 +0200

    Little complicated than the previous way to read files

---
+++ OutsideProjects/WriteAndReadFiles/WriteFiles/ReadEveryLine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadEveryLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = System.IO.File.ReadAllLines(@"c:\Users\viktor\Desktop\Homework\OutsideProjects\WriteAndReadFiles\WriteFiles\test.txt");
+            int checker = 1;
+            foreach (var item in text)
+            {
+                Console.WriteLine("{0}-line says: {1}" , checker,item );
+                checker++;
+            }
+        }
+    }
+}
