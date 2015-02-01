04e953ccad938f9132f276aede65fcd709699d84
Viktor <Viktorstanchev7@gmail.com>
2015-02-01 22:31:30 +0200

    Small program that read text file in the console

---
+++ OutsideProjects/WriteAndReadFiles/WriteFiles/ReadFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"c:\Users\viktor\Desktop\Homework\OutsideProjects\WriteAndReadFiles\WriteFiles\test.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
